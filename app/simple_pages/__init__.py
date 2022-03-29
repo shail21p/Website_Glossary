a155bedaf8b030b8f85de5a9c7e21387e545fcc9
shail patel <sp233@njit.edu>
2022-03-29 11:08:10 -0400

    requirements changes

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-from app import simple_pages
-
+simple_pages = Blueprint('simple_pages', __name__,
+                        template_folder='templates')
 
 @simple_pages.route('/')
 def index():
